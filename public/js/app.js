6828d2740e3b9639b27e1bb32d92b3d325033d30
priagungs <priagungsatyagama@gmail.com>
2018-11-04 10:20:29 +0700

    restructured directory

--- public/js/app.js
+++ public/js/app.js
@@ -4,7 +4,8 @@
     function init() {
         var router = new Router([
             new Route('beranda', 'beranda.html', true), //default
-            new Route('item', "item.html")
+            new Route('item', "item.html"),
+            new Route('profile', "user_card.html")
         ]);
     }
     init();
