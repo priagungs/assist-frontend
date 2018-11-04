9bb1cbbffe5baf29e3eac56f62728fc559fd28ae
priagungs <priagungsatyagama@gmail.com>
2018-11-04 18:23:20 +0700

    init admin dashboard

--- public/js/app.js
+++ public/js/app.js
@@ -5,7 +5,8 @@
         var router = new Router([
             new Route('beranda', 'beranda.html', true), //default
             new Route('item', "item.html"),
-            new Route('profile', "user_card.html")
+            new Route('profile', "user_card.html"),
+            new Route('admin', "admin_dashboard.html"),
         ]);
     }
     init();
