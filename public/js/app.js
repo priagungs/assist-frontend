ceeac337e7089f792b84105ae8dd9510dda16af9
rickykennedy25 <13516105@std.stei.itb.ac.id>
2018-11-12 15:14:50 +0700

    finished login and logout

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,41 @@
 'use strict';
 
+$(document).ready(() => {
+    $.ajax({
+        type: "get",
+        url: "/api/login-detail",
+        success: function (data, status) {
+            console.log(data);
+            console.log(status);
+        },
+        statusCode: {
+            401: () => {
+                console.log("not login");
+                window.location = "login.html";
+            }
+        }
+    });
+    $("#logout-button").click(() => {
+        $.ajax({
+            type: "get",
+            url: "/api/logout",
+            success: function (data, status) {
+                console.log("logout :"+status);
+                window.location = 'login.html';
+            }
+        });
+    });
+});
+
 (function () {
     function init() {
         var router = new Router([
             new Route('home', 'home.html', true), //default
-            new Route('subordinates', "subordinates.html"),
+            new Route('subordinates', "sub_detail.html"),
             new Route('profile', "user_card.html"),
             new Route('admin', "admin_dashboard.html"),
         ]);
     }
     init();
 }());
+
