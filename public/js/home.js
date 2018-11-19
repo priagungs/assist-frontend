9c31ffd6c4fe48103c2ab624881ec925cbd5249a
shintaayuck <shinta990405@gmail.com>
2018-11-19 17:35:46 +0700

    merge

--- public/js/home.js
+++ public/js/home.js
@@ -195,49 +195,4 @@ class Home {
         var content = "";
         $("#content-items").html(content);
     }
-
-
-    // changeTable() {
-        // $(".filter").change(function() {
-        //     alert("HEHE");
-        //     console.log("triggered");
-        //     var status = $(this).val();
-        //     $("#content-items").replaceWith("");
-            // $.ajax({
-            //     method: "GET",
-            //     url: "api/requests",
-            //     data: {page:this.itemPage, limit: this.itemLimit, idUser: this.idUser, status: status},
-            //     success: (response) => {
-            //         var no = 1;
-            //         var content = "";
-            //         response.content.forEach(element => {
-            //             content += '<tr class=""' + element.requestStatus + '">'
-            //             + '<td>' + no + '</td>'
-            //             + '<td>' + element.item.itemName +'</td>'
-            //             + '<td class="text-center">' + element.reqQty + '</td>'
-            //             + '<td class="text-center">' + element.requestStatus + '</td>'
-            //             + '<td> <button class="btn btn-primary btn-sm"> detail</button> </td>'
-            //             + '</tr>';
-            //             no++;
-            //         });
-            //         $("#items tbody").html(content);
-            //     }
-            // })
-          //   console.clear();
-          //   var filterValue = $(this).val();
-          //   var row = $('.row');
-          //
-          //   row.each(function(i, el) {
-          //       if ($(el).attr('data-type') == filterValue) {
-          //           row.hide()
-          //           $(el).show();
-          //       }
-          //   });
-          //   // In Addition to Wlin's Answer (For "All" value)
-          //   if ("all" == filterValue) {
-          //       row.show();
-          // }
-
-        // });
-    // }
 }
