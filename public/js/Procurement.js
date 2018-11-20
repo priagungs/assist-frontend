8c960e55c031da4514e273bbf32ed9cec4a74957
priagungs <priagungsatyagama@gmail.com>
2018-11-20 18:57:11 +0700

    finished procurement exclude invoice

--- public/js/Procurement.js
+++ public/js/Procurement.js
@@ -2,6 +2,7 @@ class Procurement {
     constructor() {
         this.page = 0;
         this.limit = 10;
+        this.dropdownLimit = 5;
     }
 
     init() {
@@ -14,8 +15,10 @@ class Procurement {
                     window.location.hash = "#home";
                 }
                 else {
+                    this.idAdmin = response.idUser;
                     this.fillTable();
                     this.detailModalHandler();
+                    this.addNewTransactionHandler();
                 }
             },
             statusCode: {
@@ -161,6 +164,197 @@ class Procurement {
                     }
                 }
             });
+        })
+    }
+
+    addNewTransactionHandler() {
+        this.resetNewTransactionTable();
+        this.itemTransactions = [];
+        $("#add-item-transaction-modal").unbind().on('show.bs.modal', () => {
+            this.resetAddItemTransactionForm();
+            this.addItemTransactionFormHandler();
+ 
+            $("#add-item-transaction-modal-btn").unbind().click(() => {
+                var itemTransaction = {
+                    item: {
+                        idItem: parseInt($("#id-item-form").text()),
+                        itemName: $("#form-add-item-transaction").val()
+                    },
+                    boughtQty: parseInt($("#form-add-item-quantity-transaction").val()),
+                    price: parseInt($("#form-add-item-price-transaction").val())
+                }
+                if (this.validateItemTransaction(itemTransaction)) {
+                    this.itemTransactions.push(itemTransaction);
+                    this.fillNewTransactionTable();
+                    $("#add-item-transaction-modal").modal('hide');
+                }
+            });
+        });
+        this.buyBtnHandler();
+
+    }
+
+    buyBtnHandler() {
+        $("#form-supplier-add-transaction").unbind().on('input', () => {
+            $("#form-supplier-add-transaction").removeClass("is-invalid");
+        })
+
+        $("#buy-btn").unbind().click(() => {
+            if (!$("#form-supplier-add-transaction").val()) {
+                $("#transaction-validation").text("Please provide a valid supplier");
+                $("#form-supplier-add-transaction").addClass("is-invalid");
+            }
+            else {
+                var request = {
+                    supplier: $("#form-supplier-add-transaction").val(),
+                    admin: {
+                        idUser: this.idAdmin
+                    },
+                    itemTransactions: this.itemTransactions
+                }
+                if (this.itemTransactions.length > 0) {
+                    $.ajax({
+                        method: "POST",
+                        url: "/api/transactions",
+                        data: JSON.stringify(request),
+                        contentType: "application/json",
+                        success: (response) => {
+                            this.fillTable();
+                            this.resetAddItemTransactionForm();
+                            this.resetNewTransactionTable();
+                            $("#nav-procurements-tab").addClass("active").attr("aria-selected", "true");
+                            $("#nav-create-procurement-tab").removeClass("active").attr("aria-selected", "false");
+                            $("#nav-procurements").addClass("active show");
+                            $("#nav-create-procurement").removeClass("active show");
+                        },
+                        statusCode: {
+                            400: () => {
+                                $("#transaction-validation").text("Bought quantity must be > 0");
+                                $("#form-supplier-add-transaction").addClass("is-invalid");
+                            }
+                        }
+                    });
+                }
+            }
+        })
+    }
+
+    validateItemTransaction(itemTransaction) {
+        var itemForm = $("#form-add-item-transaction");
+        var priceForm = $("#form-add-item-price-transaction");
+        var quantityForm = $("#form-add-item-quantity-transaction");
+
+        itemForm.unbind().on('input', () => {
+            itemForm.removeClass('is-invalid');
+        })
+        
+        priceForm.unbind().on('input', () => {
+            priceForm.removeClass('is-invalid');
+        })
+
+        quantityForm.unbind().on('input', () => {
+            quantityForm.removeClass('is-invalid');
+        })
+
+        var valid = true;
+        if (!itemTransaction.item.idItem) {
+            valid = false;
+            itemForm.addClass("is-invalid");
+        }
+        if (!itemTransaction.price) {
+            valid = false;
+            priceForm.addClass("is-invalid");
+        }
+        if (!itemTransaction.boughtQty) {
+            valid = false;
+            quantityForm.addClass("is-invalid");
+        }
+        return valid
+    }
+
+    fillNewTransactionTable() {
+        var content = ""
+        var totalPrice = 0;
+        var index = 0;
+        this.itemTransactions.forEach(element => {
+            content += '<tr><td>' + element.item.idItem + '</td>'
+            + '<td>' + element.item.itemName + '</td>'
+            + '<td>' + element.boughtQty + '</td>'
+            + '<td class="d-flex justify-content-between">Rp ' + element.boughtQty * element.price + '<i class="fa fa-times remove-item-transaction" data-index="' + index + '" aria-hidden="true"></i></td>';
+            index++;
+            totalPrice += element.boughtQty * element.price;
+        })
+        content += '<tr><td colspan="3" class="text-right">Total</td><td>Rp ' + totalPrice + '</td></tr>'
+        $("#table-new-procurement tbody").html(content);
+
+        $(".remove-item-transaction").unbind().click((event) => {
+            var removedIdx = $(event.currentTarget).data('index');
+            this.itemTransactions.splice(removedIdx, 1);
+            this.fillNewTransactionTable();
+        })
+    }
+
+    resetNewTransactionTable() {
+        $("#table-new-procurement tbody").html('');
+        $("#form-supplier-add-transaction").val('');
+        this.itemTransactions = [];
+    }
+
+    resetAddItemTransactionForm() {
+        $("#form-add-item-transaction").val('');
+        $("#form-add-item-price-transaction").val('');
+        $("#form-add-item-quantity-transaction").val('');
+        $("#id-item-form").text('');
+    }
+
+    addItemTransactionFormHandler() {
+        $("#form-add-item-transaction").unbind().focusin(() => {
+            $('#procurement .dropdown-menu').html('<p class="dropdown-item"><strong>Insert Item</strong></p>');
+        })
+        $("#form-add-item-transaction").on('input', (event) => {
+            $("#form-add-item-transaction").removeClass("is-invalid");
+            $("#id-item-form").text("");
+            if (event.target.value) {
+                $.ajax({
+                    type: "GET",
+                    url: "/api/items",
+                    data: {page: 0, limit: this.dropdownLimit, sort: "itemName", keyword: event.target.value},
+                    dataType: "json",
+                    success: (response) => {
+                        var dropdown_content = "";
+                        response.content.forEach((element) => {
+                            dropdown_content += '<button class="dropdown-item candidate-item-trx" data-iditem="' + element.idItem + '" data-name="' + element.itemName + '">'
+                            + '<div class="row"><div class="col-2">' 
+                            + '<img src="' + (element.pictureURL ? element.pictureURL : "/public/images/no-image.jpg") + '" class="img-thumbnail rounded-circle" alt=""></div>'
+                            + '<div class="col-10">'
+                            + '<p><strong>' + element.itemName + '</strong></p>'
+                            + '<p>ID : ' + element.idItem + '</p>'
+                            + '<p>Available Qty : ' + element.availableQty + '</p>'
+                            + '</div></div></button>';
+                        });
+                        if (response.content.length > 0) {
+                            $('#procurement .dropdown-menu').html(dropdown_content);
+                        }
+                        else {
+                            $('#procurement .dropdown-menu').html('<p class="dropdown-item"><strong>Item not found</strong></p>');
+                        }
+
+                        $("#dropdown-add-item-transaction .candidate-item-trx").unbind().click((event) => {
+                            event.preventDefault();
+                            $("#form-add-item-transaction").val($(event.currentTarget).data('name'));
+                            $("#id-item-form").text($(event.currentTarget).data('iditem'));
+                        })
+                    },
+                    statusCode: {
+                        401: () => {
+                            window.location = "login.html";
+                        }
+                    }
+                });
+            }
+            else {
+                $('#dropdown-add-item-transaction').html('<p class="dropdown-item"><strong>Insert Item</strong></p>');
+            }
         })
     }
 }
